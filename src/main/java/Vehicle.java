06e23e8961573e2985563949b4aaa99e9729d7b3
user <ravneets47@gmail.com>
2018-06-07 15:46:39 -0400

    Setter, getter and constructor of vehicle class created

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -5,5 +5,33 @@ public class Vehicle {
     private String plate;
     private String colour;
 
+    public Vehicle(String company, String plate, String colour) {
+        this.company = company;
+        this.plate = plate;
+        this.colour = colour;
+    }
 
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
