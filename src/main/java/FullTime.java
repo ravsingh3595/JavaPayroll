cbf356f969cc241a6c5e18bc4c5d3dae37b261ee
user <ravneets47@gmail.com>
2018-06-11 00:23:35 -0400

    fulltime and intern class created

--- src/main/java/FullTime.java
+++ src/main/java/FullTime.java
@@ -1,2 +1,46 @@
-public class FullTime {
+public class FullTime extends Employee {
+
+    private float salary;
+    private float bonus;
+
+    public FullTime()
+    {
+        super();
+    }
+
+    public FullTime(String name, int age, float salary, float bonus) {
+        super(name, age);
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public float getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(float bonus) {
+        this.bonus = bonus;
+    }
+
+    float calEarnings()
+    {
+        return (getSalary() + getBonus());
+    }
+
+    @Override
+    public void printMyData()
+    {
+        System.out.println("Employee is FullTime");
+        System.out.println("Salary:         " + getSalary());
+        System.out.println("Bonus:          " + getBonus());
+        System.out.println("Earnings:       " + calEarnings());
+    }
 }
--- src/main/java/Intern.java
+++ src/main/java/Intern.java
@@ -1,2 +1,35 @@
-public class Intern {
+public class Intern extends Employee {
+
+    private String schoolName;
+
+    public Intern()
+    {
+        super();
+    }
+
+    public Intern(String name, int age, String schoolName) {
+        super(name, age);
+        this.schoolName = schoolName;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    float calEarnings()
+    {
+        return 1000;
+    }
+
+    @Override
+    public void printMyData()
+    {
+        System.out.println("Employee is Intern");
+        System.out.println("SchoolName:     " + getSchoolName());
+        System.out.println("Earnings:       " + calEarnings());
+    }
 }
