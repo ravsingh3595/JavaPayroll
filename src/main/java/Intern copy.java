ca5affd0a98021ed39e3b323ccff1c4bc52b1841
MacStudent <macstudent@20X-iMacXX-M-478.local>
2018-06-11 16:36:01 -0400

    intern class created by nishan

---
+++ src/main/java/Intern copy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payrollsystem;
+
+/**
+ *
+ * @author macstudent
+ */
+public class Intern  {
+    	private String schoolName;
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+	
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	
+}
+
