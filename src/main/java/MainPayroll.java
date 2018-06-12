722974a4c5e35e14338d1eb2ed803412bbed44a8
user <ravneets47@gmail.com>
2018-06-12 19:35:58 -0400

    Code for getting data from the user in main class further updated

--- src/main/java/MainPayroll.java
+++ src/main/java/MainPayroll.java
@@ -4,54 +4,124 @@ import java.io.*;
 
 public class MainPayroll {
 
-    public static void main(String arg[]) throws IOException
-    {
-        InputStreamReader isr= new InputStreamReader(System.in);
-        BufferedReader br= new BufferedReader(isr);
+    public static void main(String arg[]) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
 
         System.out.println("Employee Database System");
-        CommissionBasedPartTime employee = new CommissionBasedPartTime();
-        //Employee e1 = new CommissionBasedPartTime();
+        CommissionBasedPartTime employeeC = new CommissionBasedPartTime();
+        FixedBasedPartTime empF = new FixedBasedPartTime();
+        FullTime employeeData = new FullTime();
+        Intern empData = new Intern();
+        //Employee e1 = new Employee();
 
         System.out.print("Employee Name: ");
-        employee.setName(br.readLine());
+        employeeC.setName(br.readLine());
         System.out.print("Age: ");
-        employee.setCalBirthYear(br.read());
+        String a1 = br.readLine();
+        int a = Integer.parseInt(a1);
+        employeeC.setCalBirthYear(a);
 
         System.out.print("Select your Employment type," +
                 " Choose any option from the following " +
                 "\na. Commission Based PartTime" +
                 "\nb. Fixed Based PartTime" +
                 "\nc. Intern" +
-                "\nd. FullTime" );
-        String employmentType = br.readLine();
+                "\nd. FullTime\n");
 
-        System.out.println(employmentType);
-        switch (employmentType)
-        {
+        // System.out.print("hello");
+        String employmentType = br.readLine();
+        //System.out.println(employmentType);
+        switch (employmentType) {
             case "a":
             case "A":
+
                 System.out.println("Commission Based PartTime Employee");
+                System.out.println("Enter your hourly pay:");
+                employeeC.setRate(br.read());
+                System.out.println("Enter Number of hours worked: ");
+                String a2 = br.readLine();
+                int a3 = Integer.parseInt(a1);
+                employeeC.setHoursWorked(a3);
+                System.out.println("Enter the Commission: ");
+                String a4 = br.readLine();
+                int a5 = Integer.parseInt(a4);
+                employeeC.setCommissionPercentage(a5);      //change the format to flaot right now getting int.
                 break;
+
             case "b":
             case "B":
                 System.out.println("Fixed Based PartTime Employee");
+                System.out.println("Enter your hourly pay:");
+                String a6 = br.readLine();
+                int a7 = Integer.parseInt(a6);
+                empF.setRate(a7);
+                System.out.println("Enter Number of hours worked: ");
+                String a8 = br.readLine();
+                int a9 = Integer.parseInt(a8);
+                empF.setHoursWorked(a9);
+                System.out.println("Enter your Fixed pay: ");
+                String a10 = br.readLine();
+                int a11 = Integer.parseInt(a10);
+                empF.setFixedAmount(a11);                    //change the format to flaot right now getting int.
                 break;
+
             case "c":
             case "C":
+
                 System.out.println("Intern");
+                System.out.println("Enter your School Name: ");
+                empData.setSchoolName(br.readLine());
                 break;
+
             case "d":
             case "D":
+
                 System.out.println("FullTime Employee");
+                System.out.println("Enter your Salary: ");
+                String a12 = br.readLine();
+                int a13 = Integer.parseInt(a12);
+                employeeData.setSalary(a13);
+                System.out.println("Enter the Bonus you got:");
+                String a14 = br.readLine();
+                int a15 = Integer.parseInt(a14);
+                employeeData.setBonus(a15);
                 break;
+
             default:
                 System.out.println("Incorrect Input");
                 break;
         }
+//
+//        System.out.println("What type of Vehicle you drive," +
+//                " Choose any option from the following " +
+//                "\na. Car" +
+//                "\nb. Motorcycle" +
+//                "\nc. No vehicle/ Other \n" );
+//
+//        String veh = br.readLine();
+//
+//        switch (veh)
+//        {
+//            case "a":
+//            case 'A':
+//                System.out.println("You have a Car");
+//                break;
+//            case 'b':
+//            case 'B':
+//                System.out.println("You have a MotorCycle");
+//                break;
+//            case 'c':
+//            case 'C':
+//                System.out.println("You don't like driving");
+//                break;
+//            default:
+//                System.out.println("Incorrect Input");
+//                break;
+//
+//        }break
+
     }
-
-
 }
 
 /*
